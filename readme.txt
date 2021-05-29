84bce97cd8505572df6952ce6da5684cacbc4f82
m266 <herbrand@mailbox.org>
2021-05-29 18:50:04 +0200

    Update 1.1

--- readme.txt
+++ readme.txt
@@ -1,10 +1,14 @@
 === WP H-Posts in Pages ===
 Requires at least: 5.0
-Tested up to: 5.7
+Tested up to: 5.7.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 Beiträge in Seiten einfügen
 == Installation ==
+Version 1.1
+1. PHP-Code überarbeitet
+2. Erinnerung an Git Updater kommt nun bei jeder Plugin-Aktivierung
+
 Version 1.0
 1. Initial Version
--- wphpip.php
+++ wphpip.php
@@ -1,15 +1,15 @@
 <?php
 /*
 Plugin Name:       WP H-Posts in Pages
-Plugin URI:        https://web266.de/software/eigene-plugins/wp-h-posts-in-pages/
+Plugin URI:        https://herbrand.org/wordpress/eigene-plugins/wp-h-posts-in-pages/
 Description:       Beitr&auml;ge in Seiten einf&uuml;gen
 Author:            Hans M. Herbrand
-Author URI:        https://web266.de
-Version:           1.0
-Date:              2021-03-10
+Author URI:        https://herbrand.org
+Version:           1.1
+Date:              2021-05-27
 License:           GNU General Public License v2 or later
 License URI:       http://www.gnu.org/licenses/gpl-2.0.html
-GitHub Plugin URI: https://github.com/m266/wp-h-posts-in-pages/
+GitHub Plugin URI: https://github.com/m266/wp-h-posts-in-pages
 Credits:           https://ostrich.de/wordpress-beitraege-auf-seite-anzeigen/
 Hinweis:           Tag [posts] in der Start-Seite einfuegen
 */
@@ -18,19 +18,15 @@ Hinweis:           Tag [posts] in der Start-Seite einfuegen
 defined('ABSPATH') || exit();
 
 //////////////////////////////////////////////////////////////////////////////////////////
-// Check GitHub Updater aktiv
-// Anpassungen Plugin-Name und Funktions-Name vornehmen
-if (!function_exists('is_plugin_inactive')) {
-    require_once ABSPATH . '/wp-admin/includes/plugin.php';
-}
-if (is_plugin_inactive('github-updater/github-updater.php')) {
-// E-Mail an Admin senden, wenn inaktiv
+
+// Erinnerung an Git Updater
 register_activation_hook( __FILE__, 'wphpip_activate' ); // Funktions-Name anpassen
 function wphpip_activate() { // Funktions-Name anpassen
 $subject = 'Plugin "WP H-Posts in Pages"'; // Plugin-Name anpassen
-$message = 'Bitte das Plugin "GitHub Updater" hier https://web266.de/tutorials/github/github-updater/ herunterladen, installieren und aktivieren, um weiterhin Updates zu erhalten!';
+$message = 'Falls nicht vorhanden:
+Bitte das Plugin "Git Updater" hier https://herbrand.org/tutorials/github/git-updater/ herunterladen, 
+installieren und aktivieren, um weiterhin Updates zu erhalten!';
 wp_mail(get_option("admin_email"), $subject, $message );
-}
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
